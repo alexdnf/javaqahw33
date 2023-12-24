cf173ae55e46afd58817ce16e024ead06e4f7067
Alex Deyanov <deyanovan@yandex.ru>
2023-12-24 14:07:56 +0300

    modified1

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -3,7 +3,7 @@ public class CreditPaymentService {
     public int calculate(int amount, double rate, int months) {
 
         double monthsRate = (rate / 100) / 12;
-        double payment = ((amount * monthsRate) / (1 - Math.pow(1 + monthsRate, -months)));
+        double payment = (amount * monthsRate) / (1 - Math.pow(1 + monthsRate, -months));
         return (int) payment;
     }
 }
